63939f52a26fbe7ea1d226ba89fe1929cce91fa4
Ben Browning <bbrownin@redhat.com>
2011-01-27 09:13:33 -0500

    whitespace only

--- lib/ponder/connection.rb
+++ lib/ponder/connection.rb
@@ -3,25 +3,25 @@ require 'eventmachine'
 module Ponder
   class Connection < EventMachine::Connection
     include EventMachine::Protocols::LineText2
-    
+
     def initialize(thaum)
       @thaum = thaum
     end
-    
+
     def connection_completed
       @thaum.register
     end
-    
+
     def unbind
       @thaum.connected = false
       @thaum.process_callbacks :disconnect
       @thaum.traffic_logger.info '-- Ponder disconnected'
       @thaum.console_logger.info '-- Ponder disconnected'
-      
+
       if @thaum.config.reconnect
         @thaum.traffic_logger.info "-- Reconnecting in #{@thaum.config.reconnect_interval} seconds"
         @thaum.console_logger.info "-- Reconnecting in #{@thaum.config.reconnect_interval} seconds"
-        
+
         EventMachine::add_timer(@thaum.config.reconnect_interval) do
           reconnect @thaum.config.server, @thaum.config.port
         end
@@ -29,7 +29,7 @@ module Ponder
         EventMachine::stop_event_loop
       end
     end
-    
+
     def receive_line(line)
       @thaum.parse line
     end
